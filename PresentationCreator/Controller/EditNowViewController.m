572197eeb3ab4d3bf5e297d7e2a521235ad4fd9d
linlecui <linlecui@lintekimacbook-air.local>
2015-10-22 09:36:53 +0800

    Create new

---
+++ PresentationCreator/Controller/EditNowViewController.m
@@ -0,0 +1,274 @@
+//
+//  EditNowViewController.m
+//  PresentationCreator
+//
+//  Created by songyang on 15/10/19.
+//  Copyright © 2015年 songyang. All rights reserved.
+//
+
+#import "EditNowViewController.h"
+
+@interface EditNowViewController ()<UIWebViewDelegate>
+@property (nonatomic ,strong) UIWebView *webView;
+@property (nonatomic, strong) NSString *htmlSource;
+@property (nonatomic, strong) NSString *imgIndex;
+@property (nonatomic, retain) UIImageView *imageView;
+@property (nonatomic ,strong) NSString *fullPath;
+@end
+
+@implementation EditNowViewController
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    self.parentViewController.tabBarController.tabBar.hidden = YES;
+}
+- (void)viewWillDisappear:(BOOL)animated {
+    self.parentViewController.tabBarController.tabBar.hidden = NO;
+    //    self.navigationItem.hidesBackButton =NO;//隐藏系统自带导航栏按钮
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    self.navigationItem.title= @"Edit Your Style";
+    _fullPath = [[NSString alloc]init];
+    [self addNewWebView];
+    
+}
+-(void)addNewWebView
+{
+    UIView *aView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 0, 0)];
+    [self.view addSubview:aView];
+    self.webView = [[UIWebView alloc]init];
+    self.webView.frame = CGRectMake(0, 64, KScreenWidth, KScreenHeight-64);
+    self.webView.backgroundColor = [UIColor blackColor];
+    NSString *path = [[NSBundle mainBundle]bundlePath];
+    NSURL *baseUrl = [NSURL fileURLWithPath:path];
+    [self.webView loadHTMLString:self.editNowHtmlCodeStr baseURL:baseUrl];
+    self.webView.delegate = self;
+    [self.view addSubview:self.webView];
+    [self loadHtmlToWebView];
+}
+#pragma mark - webview JSContext
+//获取webview中section里的heml代码
+- (void)getHtmlCodeClick {
+    //native  call js 代码
+    NSString *jsToGetHtmlSource =  @"document.getElementsByTagName('html')[0].innerHTML";
+    //    NSString *htmlSource = @"<section class='swiper-slide swiper-slide2'>";//slide2需要拼接获取正确的索引值
+    _htmlSource = @"<!DOCTYPE html><html>";
+    _htmlSource = [_htmlSource stringByAppendingString: [_webView stringByEvaluatingJavaScriptFromString:jsToGetHtmlSource]];
+    _htmlSource = [_htmlSource stringByAppendingString:@"</html>"];
+    //根据summaryid 和templateid查询数据库更换html_code
+    //在details表中 根据detailsid 修改html代码
+    
+    [DBDaoHelper updateDetailsIdWith:self.editNowDetailsIdStr htmlCode:_htmlSource];
+    NSLog(@"you got html is:::%@", _htmlSource);
+}
+-(void)loadHtmlToWebView{
+    
+    JSContext *context = [self.webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    
+    context[@"clickedText"] = ^() {
+        NSLog(@"%ld",(long)self.webView.tag);
+        NSLog(@"Begin text");
+        NSArray *args = [JSContext currentArguments];
+        
+        //        NSString *mySt = [args componentsJoinedByString:@","];
+        NSLog(@"input mySt:%@", args[0]);
+        NSLog(@"input mySt:%@", args[1]);
+        NSString *htmlVal = [[NSString alloc]initWithFormat:@"%@",args[0]];
+        NSString *htmlIndex =[[NSString alloc]initWithFormat:@"%@",args[1]];
+        [self editTextComponent:htmlVal :htmlIndex];
+        
+        NSLog(@"-------End Text-------");
+        
+    };
+    //点击图片js方法调用native
+    context[@"clickedImage"] = ^() {
+        NSLog(@"Begin image");
+        
+        NSArray *args = [JSContext currentArguments];
+        
+        NSLog(@"input mySt:%@", args[0]);
+        _imgIndex = [[NSString alloc]initWithFormat:@"%@",args[0]];
+        [self editImageComponent:_fullPath :_imgIndex];
+        //        [self editImageComponent: @"/Users/linlecui/Desktop/10c58PIC2CK_1024.jpg" : imgIndex];//加载本地图片到webview,把图片的索引传给方法
+        [self backgroundClick];
+        [self getHtmlCodeClick];
+        NSLog(@"-------End Image-------");
+    };
+    
+}
+//修改文字的时候
+-(void)editTextComponent:(NSString *)param1 : (NSString *)param2{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Message Alert" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField *textField){
+        
+        textField.placeholder = @"please type your message";
+        
+        textField.text = param1;
+        
+    }];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        // NSString *getTextMessage = textField.text;
+        UITextField *login = alertController.textFields.firstObject;
+        NSString *newMessage = login.text;
+        NSString *str = @"var field = document.getElementsByClassName('text_element')[";
+        str = [str stringByAppendingString:param2];
+        str = [str stringByAppendingString:@"];"];
+        str = [str stringByAppendingString:@" field.innerHTML='"];
+        str = [str stringByAppendingString:newMessage];
+        str = [str stringByAppendingString:@"';"];
+        
+        NSLog(@"final javascript:%@",str);
+        [_webView stringByEvaluatingJavaScriptFromString:str];
+        [self getHtmlCodeClick];//获取webview中section里的heml代码
+        
+    }];
+    
+    
+    [alertController addAction:cancelAction];
+    [alertController addAction:okAction];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+//更改图片
+-(void)editImageComponent:(NSString *)imgName : (NSString *)index{
+    
+    //拼接js字符串，用于替换图片
+    NSString *str = @"var field = document.getElementsByClassName('img_element')[";
+    str = [str stringByAppendingString:index];
+    str = [str stringByAppendingString:@"];"];
+    str = [str stringByAppendingString:@" field.src='"];
+    str = [str stringByAppendingString:imgName];
+    str = [str stringByAppendingString:@"';"];
+    
+    NSLog(@"final javascript:%@",str);
+    [_webView stringByEvaluatingJavaScriptFromString:str];//js字符串通过这个方法传递到webview中的html并执行此js
+}
+//选择图片
+-(void)backgroundClick
+{
+    UIActionSheet *sheet;
+    
+    // 判断是否支持相机
+    if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        sheet  = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"拍照",@"从相册选择", nil];
+    }
+    else {
+        
+        sheet = [[UIActionSheet alloc] initWithTitle:@"选择" delegate:self cancelButtonTitle:nil destructiveButtonTitle:@"取消" otherButtonTitles:@"从相册选择", nil];
+    }
+    
+    sheet.tag = 255;
+    
+    [sheet showInView:self.view];
+}
+-(void)recordClick
+{
+    
+}
+#pragma mark - 保存图片至沙盒
+- (void) saveImage:(UIImage *)currentImage withName:(NSString *)imageName
+{
+    
+    NSData *imageData = UIImageJPEGRepresentation(currentImage, 0.5);
+    // 获取沙盒目录
+    
+    _fullPath = [[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"] stringByAppendingPathComponent:imageName];
+    NSLog(@"%@",_fullPath);
+    [self loadHtmlToWebView];
+   
+     // 将图片写入文件
+    [imageData writeToFile:_fullPath atomically:NO];
+    NSArray *args = [JSContext currentArguments];
+    _imgIndex = [[NSString alloc]initWithFormat:@"%@",args[0]];
+    [self editImageComponent:_fullPath :_imgIndex];
+}
+#pragma mark - image picker delegte
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    [picker dismissViewControllerAnimated:YES completion:^{}];
+    
+    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    
+    NSString *str = [NSString stringWithFormat:@"%d.png",arc4random() % 1000000];
+    [self saveImage:image withName:str];
+    
+    NSString *fullPath = [[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"] stringByAppendingPathComponent:@"currentImage.png"];
+    UIImage *savedImage = [[UIImage alloc] initWithContentsOfFile:fullPath];
+    
+    
+    isFullScreen = NO;
+    [self.imageView setImage:savedImage];
+    
+    self.imageView.tag = 100;
+    
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:^{}];
+}
+#pragma mark - actionsheet delegate
+-(void) actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (actionSheet.tag == 255) {
+        
+        NSUInteger sourceType = 0;
+        
+        // 判断是否支持相机
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            
+            switch (buttonIndex) {
+                case 0:
+                    // 取消
+                    return;
+                case 1:
+                    // 相机
+                    sourceType = UIImagePickerControllerSourceTypeCamera;
+                    break;
+                    
+                case 2:
+                    // 相册
+                    sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                    break;
+            }
+        }
+        else {
+            if (buttonIndex == 0) {
+                
+                return;
+            } else {
+                sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+            }
+        }
+        // 跳转到相机或相册页面
+        UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+        
+        imagePickerController.delegate = self;
+        
+        imagePickerController.allowsEditing = YES;
+        
+        imagePickerController.sourceType = sourceType;
+        
+        [self presentViewController:imagePickerController animated:YES completion:^{}];
+    }
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
